36e95bcc2834720febc43cb3300effc81ae7e0ef
Shimmy-Shams <19srs12@queensu.ca>
2023-04-10 22:26:51 -0400

    Code completion and organization.

--- Main_Program/data_pre_processing.py
+++ Main_Program/data_pre_processing.py
@@ -4,10 +4,9 @@
 #
 
 # Import Statements
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn import preprocessing
+from matplotlib import pyplot as plt
+from imblearn.over_sampling import SMOTE
 from sklearn.preprocessing import StandardScaler
 
 
@@ -40,7 +39,6 @@ def moving_average_filter(df):
     window_sizes = [5, 50, 100]
     filtered_data = {}
 
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 15))
     acceleration_axes = ['Acceleration x (m/s^2)', 'Acceleration y (m/s^2)', 'Acceleration z (m/s^2)']
 
     for i, col in enumerate(acceleration_axes):
@@ -49,23 +47,42 @@ def moving_average_filter(df):
             filtered_df[col] = filtered_df[col].rolling(window=window_size).mean()
             filtered_data[f'window_size_{window_size}'] = filtered_df.dropna()
 
-            axes[i].plot(filtered_df['Time (s)'], filtered_df[col], label=f"Window size {window_size}")
+    return filtered_data
+
+
+# Plotting the filtered data
+def plot_filtered_data(df, filtered_data):
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 15))
+    acceleration_axes = ['Acceleration x (m/s^2)', 'Acceleration y (m/s^2)', 'Acceleration z (m/s^2)']
+
+    for i, col in enumerate(acceleration_axes):
+        for window_size, filtered_df in filtered_data.items():
+            axes[i].scatter(filtered_df['Time (s)'], filtered_df[col], label=f"{window_size}", s=10)
 
         # Plotting the original data
-        axes[i].plot(df['Time (s)'], df[col], label="Original data", alpha=0.3)
+        axes[i].scatter(df['Time (s)'], df[col], label="Original data", alpha=0.3, s=10)
         axes[i].set_title(col)
         axes[i].set_xlabel("Time (s)")
         axes[i].set_ylabel("Acceleration (m/s^2)")
         axes[i].legend()
 
-    plt.tight_layout()
+    #plt.tight_layout()
     #plt.show()
 
-    return filtered_data
+
+# Applying an exponential moving average to further reduce noise
+def exponential_moving_average_filter(df, alpha=0.1):
+    filtered_df = df.copy()
+    acceleration_axes = ['Acceleration x (m/s^2)', 'Acceleration y (m/s^2)', 'Acceleration z (m/s^2)']
+
+    for col in acceleration_axes:
+        filtered_df[col] = df[col].ewm(alpha=alpha).mean()
+
+    return filtered_df
 
 
 # Removing any potential outliers in the data
-def remove_outliers(data, threshold=1.5):
+def remove_outliers(data, threshold=2):
     Q1 = data.quantile(0.25, numeric_only=True)
     Q3 = data.quantile(0.75, numeric_only=True)
     IQR = Q3 - Q1
@@ -76,6 +93,13 @@ def remove_outliers(data, threshold=1.5):
     return data_filtered
 
 
+# Handling imbalance of data
+def handle_imbalance(X, y, k_neighbors=4):
+    smote = SMOTE(sampling_strategy='minority', k_neighbors=k_neighbors)
+    X_resampled, y_resampled = smote.fit_resample(X, y)
+    return X_resampled, y_resampled
+
+
 # Normalizing the data
 def normalize_data(data):
     scaler = StandardScaler()
@@ -84,18 +108,21 @@ def normalize_data(data):
     data_normalized = data.copy()
     data_normalized[columns_to_normalize] = scaler.transform(data[columns_to_normalize])
 
+    return data_normalized
+
+
+def plot_normalized_data_histograms(data_normalized):
     # Plotting the normalized data
     fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 15))
     acceleration_axes = ['Acceleration x (m/s^2)', 'Acceleration y (m/s^2)', 'Acceleration z (m/s^2)']
 
     for i, col in enumerate(acceleration_axes):
-        axes[i].plot(data_normalized['Time (s)'], data_normalized[col], label=f"Normalized {col}")
+        axes[i].hist(data_normalized[col], bins=50, alpha=0.75, label=f"Normalized {col}")
         axes[i].set_title(f"Normalized {col}")
-        axes[i].set_xlabel("Time (s)")
-        axes[i].set_ylabel("Acceleration (m/s^2)")
+        axes[i].set_xlabel("Acceleration (m/s^2)")
+        axes[i].set_ylabel("Frequency")
         axes[i].legend()
 
-    plt.tight_layout()
-    plt.show()
+    #plt.tight_layout()
+   # plt.show()
 
-    return data_normalized
--- Main_Program/main.py
+++ Main_Program/main.py
@@ -3,18 +3,21 @@
 # Course: ELEC 390
 
 # Main.py
+# JACKPOT!
+# Code that works by normalizing the data after feature extraction
 
 # Importing all the necessary libraries
 import data_extracting_features as de
 import data_pre_processing as dpp
 import data_visualization as dv
 import data_processing as dp
+import data_classifier as dc
 import pandas as pd
-import numpy as np
+import joblib
 
 
 # --------------------------------------------------------------------------------
-# --------------- Step 1 and 2: Data Collection & Storage ------------------------
+# -------------------------- Step 1: Data Collection -----------------------------
 
 # Importing the csv files from the directory
 csv_file_paths = [
@@ -30,6 +33,10 @@ meta_data_time = [
     'Data/meta_Shams/time.csv'
 ]
 
+
+# --------------------------------------------------------------------------------
+# --------------- Step 2: Data Storage and Processing ----------------------------
+
 window_size = 5  # 5 seconds
 sample_rate = 100  # Assuming 100 samples per second, adjust if necessary
 
@@ -41,61 +48,15 @@ segments = dp.segment_data(data, window_size, sample_rate)
 
 # Shuffle and create train and test splits
 train_data, test_data = dp.create_splits(segments)
-
-
-# --------------------------------------------------------------------------------
-# -------------------- Step 4: Data Preprocessing --------------------------------
-# Checking for any missing values
-train_data_file_name = "Training Data"
-test_data_file_name = "Test Data"
-
-train_data_chk_missing_values = dpp.check_nan_null_values(train_data, train_data_file_name)
-test_data_chk_missing_values = dpp.check_nan_null_values(test_data, test_data_file_name)
-
-
-train_data_handle_missing_values = dpp.handle_missing_values(train_data, method='drop')
-test_data_handle_missing_values = dpp.handle_missing_values(test_data, method='drop')
-
-
-# Applying moving average
-train_data_filtered = dpp.moving_average_filter(train_data)
-test_data_filtered = dpp.moving_average_filter(test_data)
-
-
-# Select the filtered data with window size 50
-selected_window_size = 50
-train_data_filtered_50 = train_data_filtered[f'window_size_{selected_window_size}']
-test_data_filtered_50 = test_data_filtered[f'window_size_{selected_window_size}']
-
-# Removing outliers
-train_data_no_outliers = dpp.remove_outliers(train_data_filtered_50, threshold=1.5)
-test_data_no_outliers = dpp.remove_outliers(test_data_filtered_50, threshold=1.5)
-
-# Applying normalization
-train_data_normalized = dpp.normalize_data(train_data_no_outliers)
-test_data_normalized = dpp.normalize_data(test_data_no_outliers)
-
+# Saving the Test data to test the classifier in the app
+normalized_test_data_filename = "Data/Test_data.csv"
+de.save_features_to_csv(test_data, normalized_test_data_filename)
 
 # Save data to HDF5 file
-dp.write_to_hdf5(data, csv_file_paths, train_data_normalized, test_data_normalized)
-original_data, train_data_normalized, test_data_normalized = dp.reading_from_hdf5()
-#dp.display_data(original_data, train_data_normalized, test_data_normalized)
+dp.write_to_hdf5(data, csv_file_paths, train_data, test_data)
+original_data, train_data, test_data = dp.reading_from_hdf5()
+dp.display_data(original_data, train_data, test_data)
 
-# --------------------------------------------------------------------------------
-# -------------------- Step 5: Feature Extraction --------------------------------
-# Convert the normalized data back into segments
-train_data_normalized_segments = de.convert_to_segments(train_data_normalized, window_size, sample_rate)
-test_data_normalized_segments = de.convert_to_segments(test_data_normalized, window_size, sample_rate)
-
-# Use feature extraction
-train_data_features = de.extract_features(train_data_normalized_segments)
-test_data_features = de.extract_features(test_data_normalized_segments)
-
-# Saving the extracted features into a csv file.
-train_data_filename = "Data/train_data_features.csv"
-test_data_filename = "Data/test_data_features.csv"
-
-de.save_features_to_csv(train_data_features, test_data_features, train_data_filename, test_data_filename)
 
 # --------------------------------------------------------------------------------
 # -------------------- Step 3: Data Visualization --------------------------------
@@ -104,13 +65,13 @@ de.save_features_to_csv(train_data_features, test_data_features, train_data_file
 for dataset_name in original_data.keys():
     print(f"Visualizing data for {dataset_name}")
     time, accelerationX, accelerationY, accelerationZ = dp.extract_time_and_acceleration(original_data, dataset_name)
-    #dv.plot_general_acceleration_vs_time(time, accelerationX, accelerationY, accelerationZ)
+    dv.plot_general_acceleration_vs_time(time, accelerationX, accelerationY, accelerationZ)
 
 
 # Visualization of the meta data
 df_md_time_list = [dp.process_meta_data_time(time_file) for time_file in meta_data_time]
-#dv.plot_md_time_gantt(df_md_time_list)
-#dv.plot_md_time_bar(df_md_time_list)
+dv.plot_md_time_gantt(df_md_time_list)
+dv.plot_md_time_bar(df_md_time_list)
 
 
 train_data_visualization = dv.Activity(train_data, data_source="Train")
@@ -151,3 +112,99 @@ train_data_visualization.plot_spectrogram("Walking")
 train_data_visualization.plot_spectrogram("Jumping")
 test_data_visualization.plot_spectrogram("Walking")
 test_data_visualization.plot_spectrogram("Jumping")
+
+
+# --------------------------------------------------------------------------------
+# -------------------- Step 4: Data Preprocessing --------------------------------
+# Checking for any missing values
+train_data_file_name = "Training Data"
+test_data_file_name = "Test Data"
+
+train_data_chk_missing_values = dpp.check_nan_null_values(train_data, train_data_file_name)
+test_data_chk_missing_values = dpp.check_nan_null_values(test_data, test_data_file_name)
+
+
+train_data_handle_missing_values = dpp.handle_missing_values(train_data, method='drop')
+test_data_handle_missing_values = dpp.handle_missing_values(test_data, method='drop')
+
+
+# Applying moving average
+train_data_filtered = dpp.moving_average_filter(train_data)
+test_data_filtered = dpp.moving_average_filter(test_data)
+# Plot the moving average filter
+dpp.plot_filtered_data(train_data, train_data_filtered)
+dpp.plot_filtered_data(test_data, test_data_filtered)
+
+# Select the filtered data with window size 50
+selected_window_size = 100
+train_data_filtered_100 = train_data_filtered[f'window_size_{selected_window_size}']
+test_data_filtered_100 = test_data_filtered[f'window_size_{selected_window_size}']
+
+# Applying exponential moving average
+train_data_ema_filtered = dpp.exponential_moving_average_filter(train_data_filtered_100, alpha=0.15)
+test_data_ema_filtered = dpp.exponential_moving_average_filter(test_data_filtered_100, alpha=0.15)
+
+# Removing outliers
+train_data_no_outliers = dpp.remove_outliers(train_data_ema_filtered, threshold=2)
+test_data_no_outliers = dpp.remove_outliers(test_data_ema_filtered, threshold=2)
+
+# Apply normalization to the raw training and test data sets
+train_data_normalized = dpp.normalize_data(train_data_no_outliers)
+test_data_normalized = dpp.normalize_data(test_data_no_outliers)
+dpp.plot_normalized_data_histograms(train_data_normalized)
+dpp.plot_normalized_data_histograms(test_data_normalized)
+
+
+# Convert the normalized data back into segments
+train_data_normalized_segments = de.convert_to_segments(train_data_normalized, window_size, sample_rate)
+test_data_normalized_segments = de.convert_to_segments(test_data_normalized, window_size, sample_rate)
+
+# --------------------------------------------------------------------------------
+# -------------------- Step 5: Feature Extraction --------------------------------
+# Convert the raw data into segments
+train_data_segments = de.convert_to_segments(train_data_normalized, window_size, sample_rate)
+test_data_segments = de.convert_to_segments(test_data_normalized, window_size, sample_rate)
+
+# Extract features from the segments
+train_data_features = de.extract_features(train_data_segments)
+test_data_features = de.extract_features(test_data_segments)
+
+# Saving the extracted features into a csv file.
+train_data_filename = "Data/Features/train_data_features.csv"
+test_data_filename = "Data/Features/test_data_features.csv"
+
+de.save_features_to_csv(train_data_features, train_data_filename)
+de.save_features_to_csv(test_data_features, test_data_filename)
+
+
+# --------------------------------------------------------------------------------
+# -------------------- Step 6: Train and Evaluate Classifier ---------------------
+# Extract target variable for each segment
+train_data_y = [1 if segment['Activity_Jumping'].iloc[0] == 1 else 0 for segment in train_data_segments]
+test_data_y = [1 if segment['Activity_Jumping'].iloc[0] == 1 else 0 for segment in test_data_segments]
+
+
+print("train_data_normalized_y length:", len(train_data_y))
+print("test_data_normalized_y length:", len(test_data_y))
+
+# Before SMOTE
+print("Original class distribution:")
+print(pd.Series(train_data_y).value_counts())
+
+
+# Handle class imbalance
+train_data_features, train_data_y = dpp.handle_imbalance(train_data_features, train_data_y)
+
+
+# After SMOTE
+print("Resampled class distribution:")
+print(pd.Series(train_data_y).value_counts())
+
+# Train and evaluate the classifier
+log_reg_model = dc.train_and_evaluate_logistic_regression(train_data_y, test_data_y, train_data_features, test_data_features)
+
+
+# --------------------------------------------------------------------------------
+# -------------------- Step 7: Deploying Classifier to An App ---------------------
+# Saving the logistic regression model to a file for the app
+joblib.dump(log_reg_model, 'log_reg_model.pkl')
